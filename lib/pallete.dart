1eac09881a6e84139ac4132e0075297f3def7a94
Devangana26 <devanganabarua@gmail.com>
2023-08-12 11:53:39 +0530

    added splash screen

---
+++ lib/pallete.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color splash1 = Color.fromRGBO(217, 186, 240, 1);
+  static const Color splash2 = Color.fromRGBO(126, 231, 238, 1);
+
+  static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,68 @@
+import 'package:deliveryx/homescreen.dart';
+import 'package:deliveryx/pallete.dart';
+import 'package:flutter/material.dart';
+import 'package:deliveryx/main.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => HomeScreen()));
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Pallete.splash1, Pallete.splash2],
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/delivery-girl.png',
+            height: 150,
+            width: 160,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          // Text(
+          //   'DeliveryX',
+          //   style: TextStyle(fontSize: 35),
+          // ),
+          Image.asset(
+            'assets/images/DELIVERYX.png',
+          ),
+        ],
+      ),
+    ));
+  }
+}
